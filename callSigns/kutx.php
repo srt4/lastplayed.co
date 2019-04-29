c938d6565f6d3024b556614f0d9990c80e957e90
Spencer Thomas <spthomas@expediagroup.com>
2019-04-29 14:59:34 -0700

    Add KUTX

---
+++ callSigns/kutx.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace callSigns;
+
+use model;
+
+class KUTX implements model\RadioStation {
+
+    private static $API_BASE = "https://api.composer.nprstations.org/v1/widget/50ef24ebe1c8a1369593d032/now?format=json";
+
+    private $response; 
+
+    public function __construct() 
+    {
+        $this->response = json_decode(file_get_contents(self::$API_BASE)); 
+    }
+
+    public function getCurrentSong()
+    {
+        return $this->onNow->song->trackName;
+    }
+
+    public function getCurrentArtist()
+    {
+        return $this->onNow->song->artistName;
+    }
+
+}
