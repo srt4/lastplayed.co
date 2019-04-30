242487a57ca65a4c273df49660576aa93ba1185a
Spencer Thomas <spthomas@expediagroup.com>
2019-04-29 17:26:42 -0700

    Add KZOK

---
+++ callSigns/kzok.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace callSigns;
+
+use model;
+
+class KUTX implements model\RadioStation {
+
+    private static $API_BASE = "https://us.api.iheart.com/api/v3/live-meta/stream/7787/trackHistory";
+
+    private $response; 
+
+    public function __construct() 
+    {
+        $this->response = json_decode(file_get_contents(self::$API_BASE)); 
+    }
+
+    public function getCurrentSong()
+    {
+        return $this->response->data[0]->title;
+    }
+
+    public function getCurrentArtist()
+    {
+        return $this->response->data[0]->artist;
+    }
+
+}
