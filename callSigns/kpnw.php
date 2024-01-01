e5ba2f8bc5ccdc419b62a2bba00057a223734b15
Spencer Thomas <srt4@uw.edu>
2024-01-01 12:05:48 -0800

    Imitate Chrome useragent

--- callSigns/kpnw.php
+++ callSigns/kpnw.php
@@ -2,19 +2,27 @@
 
 namespace callSigns;
 
-use model; 
+use model;
 
-class KPNW implements model\RadioStation { 
- 
-    private static $API_BASE = "https://live.989kpnw.com/wp-content/plugins/hubbard-listen-live/api/hll_cues_get.php"; 
-    
-    private $response; 
+class KPNW implements model\RadioStation {
 
-    public function __construct() 
+    private static $API_BASE = "https://live.989kpnw.com/wp-content/plugins/hubbard-listen-live/api/hll_cues_get.php";
+    private $response;
+
+    public function __construct()
     {
-        $this->response = json_decode(file_get_contents(self::$API_BASE));
+        // Set Chrome user agent header
+        $options = [
+            'http' => [
+                'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+            ]
+        ];
+        $context = stream_context_create($options);
+
+        // Make the request using the modified context
+        $this->response = json_decode(file_get_contents(self::$API_BASE, false, $context));
     }
-    
+
     public function getCurrentSong()
     {
         return $this->response->data->response[0]->data->description;
@@ -24,5 +32,5 @@ class KPNW implements model\RadioStation {
     {
         return $this->response->data->response[0]->data->artist;
     }
-  
+
 }
