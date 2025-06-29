bd8030eea3f3b045bc19862154ec9ffedddd7b5e
Spencer Thomas <spencer@lastplayed.co>
2025-06-29 00:25:52 -0700

    Update KZOK to use new iHeart Radio API endpoint

--- src/callSigns/KZOK.php
+++ src/callSigns/KZOK.php
@@ -6,23 +6,34 @@ use LastPlayed\model;
 
 class KZOK implements model\RadioStation {
 
-    private static $API_BASE = "https://live.amperwave.net/playlist/KZOK-FM-AAC";
+    private static $API_BASE = "https://kzok.iheart.com/api/v4/player/live/7787/?sc=inferno&pname=KZOK-FM&theme=light&ihrnetwork=true&embed=true";
 
     private $response; 
 
     public function __construct() 
     {
-        $this->response = json_decode(file_get_contents(self::$API_BASE)); 
+        // Set Chrome user agent header for consistency
+        $options = [
+            'http' => [
+                'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+            ]
+        ];
+        $context = stream_context_create($options);
+
+        // Make the request using the modified context
+        $this->response = json_decode(file_get_contents(self::$API_BASE, false, $context));
     }
 
     public function getCurrentSong()
     {
-        return $this->response->data[0]->title;
+        // The iHeart API returns the song title in the response
+        return $this->response->title;
     }
 
     public function getCurrentArtist()
     {
-        return $this->response->data[0]->artist;
+        // The iHeart API returns the artist in the response
+        return $this->response->artist;
     }
 
 }
