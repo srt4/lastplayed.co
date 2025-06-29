f9d8a990d674358b5a1c7d1ffb5df89897ab0cd8
Spencer Thomas <spencer@lastplayed.co>
2025-06-29 00:15:06 -0700

    Update KNDD to use new Amperwave API endpoint

--- src/callSigns/KNDD.php
+++ src/callSigns/KNDD.php
@@ -6,7 +6,7 @@ use LastPlayed\model;
 
 class KNDD implements model\RadioStation {
 
-    private static $API_BASE = "https://1077theend.radio.com/get.php?callback=_freq_tagstation_data&type=current&count=false";
+    private static $API_BASE = "https://api-nowplaying.amperwave.net/prt/nowplaying/2/5/7028/nowplaying.json";
 
     private $response; 
 
@@ -17,12 +17,14 @@ class KNDD implements model\RadioStation {
 
     public function getCurrentSong()
     {
-        return $this->response->currentEvent->title;
+        // Get the first (most recent) performance from the list
+        return $this->response->performances[0]->title;
     }
 
     public function getCurrentArtist()
     {
-        return $this->response->currentEvent->artist;
+        // Get the first (most recent) performance from the list
+        return $this->response->performances[0]->artist;
     }
 
 }
