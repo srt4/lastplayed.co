1aa47dfa3f51ef2a55c4e41d69d3a427cfd836db
Spencer Thomas <spencer@lastplayed.co>
2025-07-12 11:55:46 -0700

    Revert "feat: improve frontend responsiveness"
    This reverts commit 8a99dfd14f382385cde63003c286d37fde4af22b.

--- public/index.php
+++ public/index.php
@@ -2,13 +2,7 @@
 
 // This is a temporary autoloader. Ideally, you would run `composer install`
 // to generate a proper one.
-require __DIR__ . '/../vendor/autoload.php';
-
-use React\EventLoop\Factory;
-
-$loop = Factory::create();
-
-$autoloadAsync = function ($class) use ($loop) {
+spl_autoload_register(function ($class) {
     $prefix = 'LastPlayed\\';
     $base_dir = __DIR__ . '/../src/';
     $len = strlen($prefix);
@@ -17,17 +11,13 @@ $autoloadAsync = function ($class) use ($loop) {
     }
     $relative_class = substr($class, $len);
     $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-    $loop->futureTick(function () use ($file) {
-        if (file_exists($file)) {
-            require $file;
-        }
-    });
-};
-
-spl_autoload_register($autoloadAsync);
+    if (file_exists($file)) {
+        require $file;
+    }
+});
 
 $callSign = $_GET['call_sign'] ?? '';
 
 // Redirect to the frontend with the call sign
 header("Location: /public/index.html#/$callSign");
-exit;
+exit; 
